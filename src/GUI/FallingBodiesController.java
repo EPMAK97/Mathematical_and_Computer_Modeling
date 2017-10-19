a8b7f9283e739bf93938b8c38cbf5dcae73e960c
EPMAK97 <donskoi.ia@students.dvfu.ru>
2017-10-20 02:19:52 +1000

    add change in interface

--- src/GUI/FallingBodiesController.java
+++ src/GUI/FallingBodiesController.java
@@ -1,5 +1,116 @@
 package GUI;
 
-public class FallingBodiesController {
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.HashMap;
+import java.util.ResourceBundle;
+
+public class FallingBodiesController implements Initializable {
+    public TextField heightBody;
+    public TextField speedBody;
+    public TextField startTime;
+    public TextField finishTime;
+    public TextField numberCounts;
+    public TextField stepCounts;
+
+    public TextField densityBody;
+    public TextField densityMedium;
+    public TextField massBody;
+    public TextField volumeBody;
+    public TextField radiusBody;
+
+    public ComboBox<String> medium;
+    public ComboBox<String> materialBody;
+
+    public Button compute;
+
+    private static HashMap<String, Double> materialDensity;
+
+    static {
+        materialDensity = new HashMap<String, Double>() {{
+           put("Воздух",    1.2);
+           put("Водород",   0.09);
+           put("Гелий",     0.18);
+           put("Глицерин",  1260.0);
+           put("Оливковое масло", 920.0);
+           put("Поливинилхлорид", 1400.0);
+           put("Вода",      1000.0);
+
+           put("Дерево", 500.0);
+           put("Стекло", 1200.0);
+           put("Резина", 900.0);
+           put("Камни",  2200.0);
+           put("Сталь",  7800.0);
+           put("Алюминий", 2700.0);
+           put("Свинец",   11300.0);
+           put("Серебро",  10600.0);
+           put("Золото",   19300.0);
+        }};
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        medium.getItems().setAll("Воздух",
+                "Водород",
+                "Вода",
+                "Гелий",
+                "Глицерин",
+                "Оливковое масло",
+                "Поливинилхлорид");
+
+        materialBody.getItems().setAll("Дерево",
+                "Стекло",
+                "Резина",
+                "Камни",
+                "Сталь",
+                "Алюминий",
+                "Свинец",
+                "Серебро",
+                "Золото");
+
+    }
+
+    public void setDensityMedium(String s) {
+        densityMedium.setText(s);
+    }
+
+    public void setDensityBody(String s) {
+        densityBody.setText(s);
+    }
+
+    public void setMaterialBody() {
+        if (materialBody.getValue() != null) {
+            if (materialDensity.containsKey(materialBody.getValue()))
+                setDensityMedium(String.valueOf(materialDensity.get(materialBody.getValue())));
+        }
+    }
+
+    public void setMedium() {
+        if (medium.getValue() != null) {
+            if (materialDensity.containsKey(medium.getValue()))
+                setDensityBody(String.valueOf(materialDensity.get(medium.getValue())));
+        }
+    }
+
+    public void checkBoxSelectedF_A() {
+        // Сила Архимеда
+    }
+
+    public void checkBoxSelectedF_C1() {
+        // Сила с линейной зависимостью
+    }
+
+    public void checkBoxSelectedF_C2() {
+        // Сила с квадратичной зависимостью
+    }
+
+    public void computeFromParameters() {
+        // Считать все и посчитать
+    }
 
 }
--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,28 +1,18 @@
 package Main;
 
-import Equation.CoffeeCoolingProcess;
-import Graphics.MatlabChart;
-import Graphics.SomeChart;
-import NumericalMethods.Adam_BashforthMethod;
-import NumericalMethods.Adam_MoultonMethod;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import org.knowm.xchart.SwingWrapper;
-import org.knowm.xchart.XYChart;
-
-import javax.swing.*;
-import java.util.ArrayList;
 
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../GUI/CoffeeCooling.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../GUI/FallingBodies.fxml"));
         primaryStage.setTitle("Математическое и компьютерное моделирование");
         Scene scene = new Scene(root, 580, 400);
         primaryStage.setScene(scene);
@@ -31,7 +21,7 @@ public class Main extends Application {
     }
 
     public static void main(String[] args) {
-        //launch(args);
+        launch(args);
         //
         //try {
         //    System.out.println(Experiment.Experiment.GetBestR());
@@ -40,40 +30,6 @@ public class Main extends Application {
         //    System.out.println(e.toString());
         //}
 
-        Double x0 = 0.0;
-        Double x1 = 5.0;
-        Double y0 = 0.0;
-        Integer N = 20;
 
-        CoffeeCoolingProcess coffeeCoolingProcess = new CoffeeCoolingProcess();
-        coffeeCoolingProcess.setXStart(x0);
-        coffeeCoolingProcess.setXFinish(x1);
-        coffeeCoolingProcess.setT0(y0);
-        coffeeCoolingProcess.setN(N);
-
-        ArrayList<ArrayList<Double>> solutions = new ArrayList<>();
-        ArrayList<ArrayList<Double>> errors = new ArrayList<>();
-        ArrayList<String> names = new ArrayList<>();
-
-        for (int i = 2; i <= 4; ++i) {
-            Adam_BashforthMethod.Solve(coffeeCoolingProcess, i, N, x0, x1, y0);
-            solutions.add(coffeeCoolingProcess.getY());
-            names.add(String.format("Adam-Bashforth %d order", i));
-            errors.add(coffeeCoolingProcess.getAbsoluteErrors());
-
-            Adam_MoultonMethod.Solve(coffeeCoolingProcess, i, N, x0, x1, y0);
-            solutions.add(coffeeCoolingProcess.getY());
-            names.add(String.format("Adam-Moulton %d order", i));
-            errors.add(coffeeCoolingProcess.getAbsoluteErrors());
-        }
-
-        SomeChart<XYChart> chartMatlab = new MatlabChart();
-        XYChart chartSolutions = chartMatlab.getChart(coffeeCoolingProcess.getX(), solutions, names);
-        chartSolutions.setTitle("Adam-Bashforth Method");
-        new SwingWrapper<>(chartSolutions).displayChart().setDefaultCloseOperation(WindowConstants.HIDE_ON_CLOSE);
-
-        XYChart chartErrors = chartMatlab.getChart(coffeeCoolingProcess.getX(), errors, names);
-        chartErrors.setTitle("Absolute errors");
-        new SwingWrapper<>(chartErrors).displayChart().setDefaultCloseOperation(WindowConstants.HIDE_ON_CLOSE);
     }
 }
