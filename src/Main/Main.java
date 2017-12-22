c0bd4fb5171b304851166fc884ac93294d9d8e16
EPMAK97 <donskoi.ia@students.dvfu.ru>
2017-12-22 14:23:56 +1000

    add new Lab

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../GUI/Oscillator_1D.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../GUI/Terver.fxml"));
         primaryStage.setTitle("Математическое и компьютерное моделирование");
         Scene scene = new Scene(root, 919, 551);
         primaryStage.setScene(scene);
